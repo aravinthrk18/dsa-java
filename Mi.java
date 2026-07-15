660e12032bfa64f62e35370428f6e820ac02e557
aravinthrk18 <aravinthkannan6669@gmail.com>
2026-07-15 10:37:12 +0530

    anagram

---
+++ Mi.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class 
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	     String[] words = {
+                "eat", "tea", "tan", "ate", "nat", "bat"
+        };
+        
+        groupAnagrams(words);
+	    
+	    
+	    
+	    
+	    
+	
+	
+	
+}
+ static void groupAnagrams(String[] words) {
+     
+     HashMap<String , ArrayList<String>> map = new HashMap<>();
+     
+     for(int i=0;i<words.length;i++){
+         
+         char ch[] = words[i].toCharArray();
+         Arrays.sort(ch);
+         
+         String key = new String(ch);
+         Arrays.sort(ch);
+         
+         if(!map.containsKey(key)){
+             map.put(key , new ArrayList<>());
+         }
+         
+         map.get(key).add(words[i]);
+     }
+     
+     
+     for(ArrayList<String> group : map.values()){
+         System.out.print(group);
+     }
+}
+}
